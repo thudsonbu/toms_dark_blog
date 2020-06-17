c00cf7f26c32acb3411674ab56711a0d697bd662
thudsonbu <hudson_thom@bentley.edu>
2020-06-17 19:10:55 +0000

    First Blog App Commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// App config
+var express     = require("express");
+var app         = express();
+var bodyParser  = require("body-parser");
+var mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/blogApp", {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose config
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Routes
+
+// INDEX
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log("error");
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	})
+});
+
+// NEW
+app.get("/blogs/new", function(req,res){
+	res.render("new");
+});
+
+// CREATE
+app.post("/blogs", function(req, res){
+	// create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		} else {
+			// redirect to blogs if succesful
+			res.redirect("/blogs");
+		}
+	})
+});
+
+app.listen(3000, function() { 
+  console.log('Blog app server listening on port 3000.'); 
+});
